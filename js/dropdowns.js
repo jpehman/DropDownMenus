a8b6eb907e7cb14ae951762e02b386ad16093d98
jpehman <jpehman@gmail.com>
2015-08-13 21:59:04 -0500

    added example.html file, jquery, and css folders/files

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -4,13 +4,13 @@
 	function DropDownMenus (configObj) {
 		var dropDown = this;
 		try {
-			this.menus = document.querySelector("div.dd-menu");
+			this.menus = document.querySelectorAll(".dd-menu");
 
 			if (this.menus.length < 1) {
 				throw "no dd-menu classes found in the document";
 			}
 
-			this.targets = document.querySelector(".dd-menu-target");
+			this.targets = document.querySelectorAll(".dd-menu-target");
 			
 			if (this.targets.length < 1) {
 				throw "no dd-menu-target classes found in the document";
@@ -21,7 +21,7 @@
 			console.log(ex);
 		}
 
-		var menuOpen = null, timer = configObj.timeout, menuTimeout = null;
+		var menuOpen = null, hideTimer = (configObj.timeout ? configObj.timeout : 4000), menuTimeout = null;
 
 		this.getTargetMenu = function (tgt) {
 			var i = this.menus.length, tgtClasses = tgt.className;
@@ -47,22 +47,35 @@
 		},
 
 		setMenuPosition = function (tgt, ddMenu) {
-			ddMenu.style.position = "fixed";
-				 
+			var left = $(tgt).position().left,
+			$ddMenu = $(ddMenu);
+
+			$ddMenu.css({
+				"position": "fixed",
+			}).width("auto");
+
+			while ($ddMenu.width() + left > $(window).width()) {
+				left -= 2;
+			}
+
+			$ddMenu.css("left", left);
 		},
 
 		showMenu = function (ddMenu) {
-			
+			ddMenu.style.display = "block";
+			menuOpen = ddMenu;
 		},
 
 		tgtClickFunc = function (event) {
 			var src = event.target ? event.target : event.srcElement,
 			menu = dropDown.getTargetMenu(src);
-
+			if (menuOpen !== null && menu.id == menuOpen.id) {
+				return hideMenu();
+			}
 			setMenuPosition(src, menu);
 			showMenu(menu);
 
-			menuTimeout = setTimeout(hideMenu, dropDown.timer);
+			menuTimeout = setTimeout(hideMenu, hideTimer);
 		},
 
 		docClickFunc = function (event) {
@@ -70,21 +83,26 @@
 		},
 
 		menuMouseLeaveFunc = function (event) {
-			menuTImeout = setTimeout(hideMenu, 1500);
+			menuTimeout = setTimeout(hideMenu, 1000);
 		},
 
 		menuMouseEnterFunc = function (event) {
 			clearMenuTimeout();
 		};
 
-		this.init = function () {
-			var idx = this.targets.length;
-			while (idx--) {
-				dropDown.targets[idx].onclick = tgtClickFunc;
-			}
-		};
+		var idx = this.targets.length;
+		while (idx--) {
+			dropDown.targets[idx].onclick = tgtClickFunc;
+		}
 
+		idx = this.menus.length;
+		while (idx--) {
+			dropDown.menus[idx].onmouseenter = menuMouseEnterFunc;
+			dropDown.menus[idx].onmouseleave = menuMouseLeaveFunc;
+		}
 	}
 
+	window.DropDownMenus = DropDownMenus;
 
+	new DropDownMenus({});
 }());
